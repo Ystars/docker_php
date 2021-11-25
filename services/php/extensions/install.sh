2b1ebb0ef3d0eaa7f66259c2ed845734348e65fb
yzai <starguoxiaoying@hotmail.com>
2021-11-25 17:27:55 +0800

    add PHP extensions

--- services/php/extensions/install.sh
+++ services/php/extensions/install.sh
@@ -16,12 +16,52 @@ fi
 
 handle() {
 
+    apecial=swoole
+
     for line in ${PHP_EXTENSIONS//,/' '}
     do
+
+        for ape in ${apecial//,/' '}
+        do
+            if [ "$line" = "$ape" ]; then
+                ${ape}
+                continue 2
+            fi
+        done
+        
+
         echo "======= install ${line} ======="
         docker-php-ext-install ${MC} ${line}
+
     done
 
+}
+
+#
+# Install extension from package file(.tgz),
+# For example:
+#
+# installExtensionFromTgz swoole-4.8.2
+#
+# Param 1: Package name with version
+# Param 2: enable options
+#
+installExtensionFromTgz()
+{
+    tgzName=$1
+    extensionName="${tgzName%%-*}"
+
+    mkdir ${extensionName}
+    tar -xf ${tgzName}.tgz -C ${extensionName} --strip-components=1
+    ( cd ${extensionName} && phpize && ./configure && make ${MC} && make install )
+
+    docker-php-ext-enable ${extensionName} $2
+}
+
+swoole() {
+    echo "---------- Install swoole ----------"
+
+    installExtensionFromTgz swoole-4.8.2
 }
 
 handle
